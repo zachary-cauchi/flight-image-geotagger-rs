cd3b543d2fb47e213b66a3c142a834e7a3dc7e3b
Zachary Cauchi <zach.cauchi@hotmail.com>
2024-12-30 12:40:57 +0100

    fix: timezone offset not being applied

--- src/image_geotagger.rs
+++ src/image_geotagger.rs
@@ -3,7 +3,7 @@ use std::{
     path::{Path, PathBuf},
 };
 
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, FixedOffset, TimeZone, Utc};
 use exif::{Exif, Field, In, Tag, Value};
 use img_parts::{jpeg::Jpeg, ImageEXIF};
 
@@ -36,36 +36,56 @@ impl ImageGeotagger {
     }
 
     fn get_image_timestamp(&self, exif: &Exif) -> GTResult<DateTime<Utc>> {
-        let field = exif
+        let datetime_original = exif
             .get_field(Tag::DateTimeOriginal, In::PRIMARY)
             .ok_or(GTError::MissingData("DateTimeOriginal".to_string()))?;
 
-        let Value::Ascii(ref val) = field.value else {
+        let offset_original = exif
+            .get_field(Tag::OffsetTimeOriginal, In::PRIMARY)
+            .ok_or(GTError::MissingData("OffsetTimeOriginal".to_string()))?;
+
+        let Value::Ascii(ref datetime_original) = datetime_original.value else {
             return Err(GTError::MissingData(
                 "DateTimeOriginal not formatted correctly.".to_string(),
             ));
         };
 
-        let datetime = exif::DateTime::from_ascii(&val[0])?;
+        let Value::Ascii(ref offset_original) = offset_original.value else {
+            return Err(GTError::MissingData(
+                "OffsetTimeOriginal not formatted correctly.".to_string(),
+            ));
+        };
+
+        let mut datetime_original = exif::DateTime::from_ascii(&datetime_original[0])?;
+        datetime_original.parse_offset(&offset_original[0])?;
 
         let naive_date = chrono::NaiveDate::from_ymd_opt(
-            datetime.year.into(),
-            datetime.month.into(),
-            datetime.day.into(),
+            datetime_original.year.into(),
+            datetime_original.month.into(),
+            datetime_original.day.into(),
         )
         .ok_or(GTError::InvalidData(
             "Invalid date initialisation".to_string(),
         ))?;
         let naive_time = chrono::NaiveTime::from_hms_opt(
-            datetime.hour.into(),
-            datetime.minute.into(),
-            datetime.second.into(),
+            datetime_original.hour.into(),
+            datetime_original.minute.into(),
+            datetime_original.second.into(),
         )
         .ok_or(GTError::InvalidData(
             "Invalid time initialisation".to_string(),
         ))?;
 
-        Ok(chrono::NaiveDateTime::new(naive_date, naive_time).and_utc())
+        let naive_datetime = chrono::NaiveDateTime::new(naive_date, naive_time);
+        let adjusted_datetime = datetime_original
+            .offset
+            .and_then(|min| FixedOffset::east_opt((min * 60).into()))
+            .and_then(|offset| offset.from_local_datetime(&naive_datetime).single())
+            .ok_or(GTError::Conversion(
+                "Failed to parse timezone offset.".to_string(),
+            ))?;
+
+        Ok(adjusted_datetime.with_timezone(&Utc))
     }
 
     fn build_new_exif<'a>(&self, exif: &'a Exif, new_fields: &'a [Field]) -> Vec<&'a Field> {
@@ -104,6 +124,10 @@ impl ImageGeotagger {
         let exif = self.load_image_exif(&jpeg)?;
 
         let timestamp = self.get_image_timestamp(&exif)?;
+
+        for field in exif.fields() {
+            println!("Tag: {}, Value: {field:?}", field.tag);
+        }
 
         println!("Done.\nGetting new GPS metadata.");
         let new_fields = self.flight_data.get_gps_exif_from_datetime(timestamp)?;
